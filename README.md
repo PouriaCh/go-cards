a92d8e2c76ff72612c99b9f0e93ba304ec51c05c
Pouria Chalangari <pouria.chalangari@gmail.com>
2024-02-24 19:26:44 -0500

    create newdeck type

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # go-cards
-A Go project that resembles a normal deck of cards
+
+A Golang project that resembles a normal deck of cards.
---
+++ deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
